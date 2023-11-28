51f99820da5e58ef19ce6929946e6566f77a18cd
Humaira9139 <humairabinteshehzad@gmail.com>
2023-11-28 10:21:35 +0000

    initial commit

---
+++ src/main/java/jobAdvertising/CommandLineAppStartupRunner.java
@@ -0,0 +1,33 @@
+package jobAdvertising;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import jobAdvertising.service.EmailService;
+
+import java.util.Scanner;
+
+@Component
+public class CommandLineAppStartupRunner implements CommandLineRunner {
+
+    private final EmailService emailService;
+
+    public CommandLineAppStartupRunner(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter sender's email: ");
+        String sender = scanner.nextLine();
+        System.out.println("Enter recipient's email: ");
+        String recipient = scanner.nextLine();
+        System.out.println("Enter subject: ");
+        String subject = scanner.nextLine();
+        System.out.println("Enter email body: ");
+        String body = scanner.nextLine();
+
+        emailService.sendEmail(sender, recipient, subject, body); // Use the emailService instance
+    }
+
+}
---
+++ src/main/java/jobAdvertising/domain/email.java
@@ -0,0 +1,50 @@
+package jobAdvertising.domain;
+
+public class email {
+        private Long id;
+        private String sender;
+        private String receiver;
+        private String subject;
+        private String body;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
+
