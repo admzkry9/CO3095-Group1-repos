06b759f0286c0b22f2eb23146e720bf752d7421b
banuarioz <ba217@student.le.ac.uk>
2023-11-30 15:17:35 +0000

    Banu feedback page and schedule

---
+++ src/main/java/jobAdvertising/domain/Feedback.java
@@ -0,0 +1,23 @@
+package jobAdvertising.domain;
+
+public class Feedback {
+        private String feedbackText;
+        private String reply;
+
+        public String getFeedbackText() {
+            return feedbackText;
+        }
+
+        public void setFeedbackText(String feedbackText) {
+            this.feedbackText = feedbackText;
+        }
+
+        public String getReply() {
+            return reply;
+        }
+
+        public void setReply(String reply) {
+            this.reply = reply;
+        }
+    }
+
