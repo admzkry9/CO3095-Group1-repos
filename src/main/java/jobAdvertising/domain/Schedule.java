e17be80f14c580148e3e29db16364bd55b6a7568
banuarioz <ba217@student.le.ac.uk>
2023-11-30 15:29:10 +0000

    Banu feedback page and schedule

---
+++ src/main/java/jobAdvertising/domain/Schedule.java
@@ -0,0 +1,21 @@
+package jobAdvertising.domain;
+
+public class Schedule {
+    private String dateWithEmployer;
+    public String getDateWithEmployer;
+    public String getDateWithEmployer() {
+        return dateWithEmployer;
+    }
+
+    public void setDateWithEmployer(String dateWithEmployer) {
+        this.dateWithEmployer = dateWithEmployer;
+    }
+
+    public String getGetDateWithEmployer() {
+        return getDateWithEmployer;
+    }
+
+    public void setGetDateWithEmployer(String getDateWithEmployer) {
+        this.getDateWithEmployer = getDateWithEmployer;
+    }
+}
