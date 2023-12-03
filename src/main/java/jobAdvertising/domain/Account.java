91c52d6b9395c3e0cb1e1cbad08815b690f6aab7
Adm Zk <admzkry9@gmail.com>
2023-12-03 04:34:42 +0000

    login function

--- src/main/java/jobAdvertising/domain/Account.java
+++ src/main/java/jobAdvertising/domain/Account.java
@@ -1,2 +1,37 @@
-package jobAdvertising.domain;public class Account {
+package jobAdvertising.domain;
+
+public class Account {
+
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String role;
+
+    private String admin_name = "group1admin";
+
+    private String admin_pass = "password123";
+
+    public Long getId() { return id;}
+
+    public void setId(Long id){this.id = id;}
+
+    public String getUsername() { return username;}
+
+    public void setUsername(String username) {this.username = username;}
+
+    public String getPassword() { return password;}
+
+    public void setPassword(String password) {this.password = password;}
+
+    public String getRole() { return role;}
+
+    public void setRole(String username) {this.role = role;}
+
+    public String getAdmin_name() { return admin_name;}
+
+    public String getAdmin_pass() { return admin_pass;}
+
 }
--- src/main/java/jobAdvertising/service/AccountService.java
+++ src/main/java/jobAdvertising/service/AccountService.java
@@ -1,2 +1,66 @@
-package jobAdvertising.service;public class AccountService {
+package jobAdvertising.service;
+
+import java.util.Scanner;
+import jobAdvertising.domain.Account;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+
+    public Account AccountCreate(){
+
+        Scanner scanner = new Scanner(System.in);
+        Account account = new Account();
+        boolean ValidAns = false;
+
+        System.out.println("Create a username : ");
+        account.setUsername(scanner.nextLine());
+
+        System.out.println("Create a password : ");
+        account.setPassword(scanner.nextLine());
+
+        while(ValidAns == false){
+
+            System.out.println("Create a username : ");
+            String RoleAns = scanner.nextLine();
+            if(RoleAns.equals("applicant") || RoleAns.equals("employer")){
+                account.setRole(RoleAns);
+                ValidAns = true;
+            }else{
+                System.out.println("Invalid Role, Please Try again");
+                continue;
+            }
+        }
+
+
+
+        return account;
+    }
+
+    public Account AccountLogin(){
+
+        Scanner scanner = new Scanner(System.in);
+        Account account = new Account();
+        boolean ValidAns = false;
+        while(ValidAns == false) {
+
+            System.out.println("Login Page");
+            System.out.println("Enter Username : ");
+            account.setUsername(scanner.nextLine());
+
+            System.out.println("Enter Password : ");
+            account.setPassword(scanner.nextLine());
+
+            if (account.getUsername().equals(account.getAdmin_name()) && account.getPassword().equals(account.getAdmin_pass())) {
+                System.out.print("Welcome Admin");
+                break;
+            } else{
+                System.out.print("Incorrect Username or Password");
+                continue;
+
+            }
+        }
+
+        return account;
+    }
 }
