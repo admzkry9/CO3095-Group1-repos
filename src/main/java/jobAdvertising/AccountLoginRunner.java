8fb1ed9fb69cb1752fa97d473e05326e8847ad68
Adm Zk <admzkry9@gmail.com>
2023-12-06 03:38:17 +0000

    Optimized Account service

--- src/main/java/jobAdvertising/AccountLoginRunner.java
+++ src/main/java/jobAdvertising/AccountLoginRunner.java
@@ -1,2 +1,25 @@
-package jobAdvertising;public class AccountLoginRunner {
+package jobAdvertising;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import jobAdvertising.service.AccountService;
+
+@Component
+public class AccountLoginRunner implements CommandLineRunner {
+
+    private final AccountService accountService;
+
+    public AccountLoginRunner(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        accountService.AccountLogin();
+        System.out.println();
+    }
+
+
+
+
 }
